35f5b20f1c9069f4732f2b12228e13a6467f6171
Luis Hdez <luis.munoz.hdez@gmail.com>
2013-01-11 04:57:42 +0100

    Set empty password as default

--- manifests/user.pp
+++ manifests/user.pp
@@ -2,7 +2,7 @@
 #
 #
 define rabbitmq::user (
-  $password,
+  $password            = '',
   $tags                = '',
   $version             = $rabbitmq::version,
   $service_autorestart = $rabbitmq::bool_service_autorestart,
