895f2d1522c7b7d899bb300ea25ab0ba9f7b80e8
Luis Hdez <luis.munoz.hdez@gmail.com>
2013-01-11 03:58:18 +0100

    Add permissions managment

---
+++ manifests/permissions.pp
@@ -0,0 +1,31 @@
+# = Define: rabbitmq::permissions
+#
+#
+define rabbitmq::permissions (
+  $vhost               = false,
+  $conf                = "^$name-.*",
+  $write               = ".*",  
+  $read                = ".*",
+  $package             = $rabbitmq::package,
+  ) {
+
+  if $vhost {
+    $vhost_option = "-p $vhost";
+  }
+
+  if $enseure == 'absent' {
+
+    exec { "rabbitmq-user-$name":
+      command => "rabbitmqctl clear_permissions $vhost_option $name",
+      require => Package[$package],
+    }
+
+  } else {
+
+    exec { "rabbitmq-user-$name":
+      command => "rabbitmqctl set_permissions $name \"$conf\" \"$write\" \"$read\"",
+      require => Package[$package],
+    }
+
+  }
+}
