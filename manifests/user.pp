9a5ac9016a8cf7d680ac5612ba19c32a8ba34902
Luis Hdez <luis.munoz.hdez@gmail.com>
2013-01-11 03:44:44 +0100

    Add user managment

--- manifests/user.pp
+++ manifests/user.pp
@@ -2,17 +2,33 @@
 #
 #
 define rabbitmq::user (
-  $admin,
   $password,
+  $tags                = false,
   $version             = $rabbitmq::version,
   $service_autorestart = $rabbitmq::bool_service_autorestart,
-  $service             = $rabbitmq::service,
-  $absent              = $rabbitmq::absent
+  $package             = $rabbitmq::package,
   ) {
 
-  $manage_service_autorestart = $service_autorestart ? {
-    true    => "Service[$service]",
-    false   => undef,
-  }
+  if $enseure == 'absent' {
 
+    exec { "rabbitmq-user-$name":
+      command => "rabbitmqctl delete_user $name",
+      require => Package[$package],
+    }
+
+  } else {
+
+    exec { "rabbitmq-user-$name":
+      command => "rabbitmqctl add_user $name $password",
+      require => Package[$package],
+    }
+
+    if $tags {
+      exec { "rabbitmq-user-tags-$name":
+        command => "rabbitmqctl set_user_tags $name $tags",
+        require => Package[$package],
+      }
+    }
+
+  }
 }
