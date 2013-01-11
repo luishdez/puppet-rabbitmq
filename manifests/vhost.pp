6a79f16b416894f4d08c70138e24e7c661e579f1
Luis Hdez <luis.munoz.hdez@gmail.com>
2013-01-11 03:50:54 +0100

    Add vhost managment

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -1,19 +1,23 @@
-# = Define: php::module
+# = Define: rabbitmq::vhost
 #
 #
 define rabbitmq::vhost (
-  $version             = $rabbitmq::version,
-  $service_autorestart = $rabbitmq::bool_service_autorestart,
-  $service             = $rabbitmq::service,
-  $absent              = $rabbitmq::absent
+  $package             = $rabbitmq::package,
   ) {
 
-  $manage_service_autorestart = $service_autorestart ? {
-    true    => "Service[$service]",
-    false   => undef,
+  if $enseure == 'absent' {
+
+    exec { "rabbitmq-user-$name":
+      command => "rabbitmqctl delete_vhost $name",
+      require => Package[$package],
+    }
+
+  } else {
+
+    exec { "rabbitmq-user-$name":
+      command => "rabbitmqctl add_vhost $name",
+      require => Package[$package],
+    }
+
   }
-
-  # notify  => $manage_service_autorestart,
-
-
 }
